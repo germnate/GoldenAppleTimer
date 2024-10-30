4e5e9b54eef9a57ce3a581a8c85fc0b7dce18e9f
Nathan Bustamante <bustamanten2@gmail.com>
2024-10-30 18:31:28 -0400

    add study-break-longbreak cycle

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,13 +1,78 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { StudySwitch, STATUSES } from "./StudySwitch";
 
 export function Timer(props) {
-    const [activeTimer, setActiveTimer] = useState(props.studyTime);
-    const [studyStatus, setStudyStatus] = useState('')
-    const [timer, setTimer] = useState(null);
+    const initialState = {
+        activeTimer: props.studyTime,
+        studyStatus: null,
+        interval: null,
+        numStudies: 0,
+    }
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        if (!state.interval && state.activeTimer?.minutes && state.studyStatus) {
+            return start();
+        }
+        if (!state.activeTimer || state.activeTimer.minutes > 0 || state.activeTimer.seconds > 0) {
+            return;
+        }
+        dispatch({ type: 'NEXT' })
+
+    }, [state.activeTimer])
+
+    function reducer(state, action) {
+        switch (action.type) {
+            case "ACTIVE_TIMER":
+                return {
+                    ...state,
+                    activeTimer: action.activeTimer
+                }
+            case "STUDY_STATUS":
+                return {
+                    ...state,
+                    studyStatus: action.studyStatus
+                }
+            case "INTERVAL":
+                return {
+                    ...state,
+                    interval: action.interval,
+                }
+            case "PAUSE":
+                clearInterval(state.interval)
+                return {
+                    ...state,
+                    interval: null,
+                }
+            case 'NEXT':
+                clearInterval(state.interval)
+                let nextStatus = null;
+                let nextNumStudies = state.numStudies;
+                if (state.studyStatus === STATUSES.study) {
+                    if (state.numStudies > 2) {
+                        nextNumStudies = 0;
+                        nextStatus = STATUSES.longBreak;
+                    } else {
+                        nextNumStudies += 1;
+                        nextStatus = STATUSES.break;
+                    }
+                }
+                if (state.studyStatus === STATUSES.break) nextStatus = STATUSES.study;
+                if (state.studyStatus === STATUSES.longBreak) nextStatus = STATUSES.study;
+                return {
+                    ...state,
+                    interval: null,
+                    studyStatus: nextStatus,
+                    activeTimer: getTime(nextStatus),
+                    numStudies: nextNumStudies,
+                }
+            default:
+                throw new Error(`${action.type} not implemented`)
+        }
+    }
 
     function getTime(name) {
-        switch (name || studyStatus) {
+        switch (name || state.studyStatus) {
             case STATUSES.study:
                 return props.studyTime
             case STATUSES.break:
@@ -21,36 +86,36 @@ export function Timer(props) {
 
     function getDisplayValue() {
         return <>
-            <span>{activeTimer.minutes.toString().padStart(2, '0')}</span>
+            <span>{state.activeTimer.minutes.toString().padStart(2, '0')}</span>
             :
-            <span>{activeTimer.seconds.toString().padStart(2, '0')}</span>
+            <span>{state.activeTimer.seconds.toString().padStart(2, '0')}</span>
         </>
     }
 
     function pause() {
-        clearInterval(timer);
-        setTimer(null);
+        dispatch({ type: 'PAUSE' })
     }
 
     function start() {
-        if (!studyStatus) setStudyStatus(STATUSES.study)
-        let min = activeTimer.minutes;
-        let sec = activeTimer.seconds;
+        console.log('START!', state)
+        if (!state.studyStatus) dispatch({ type: 'STUDY_STATUS', studyStatus: STATUSES.study })
+        let min = state.activeTimer.minutes;
+        let sec = state.activeTimer.seconds;
         const interval = setInterval(() => {
-            if (min === 0 && sec === 0) return clearInterval(interval);
+            if (min === 0 && sec === 0) return pause();
             sec = sec - 1;
             if (sec < 0) {
                 min = min - 1;
                 sec = 59;
             }
-            setActiveTimer({ minutes: min, seconds: sec })
-        }, 1000)
-        setTimer(interval)
+            dispatch({ type: 'ACTIVE_TIMER', activeTimer: { minutes: min, seconds: sec } })
+        }, 100)
+        dispatch({ type: 'INTERVAL', interval })
     }
 
     function reset(name) {
         pause();
-        setActiveTimer(getTime(name))
+        dispatch({ type: 'ACTIVE_TIMER', activeTimer: getTime(name) })
     }
 
     return (
@@ -58,14 +123,14 @@ export function Timer(props) {
             <div className='timer'>
                 <div className='time'>{getDisplayValue()}</div>
                 <div className='buttons'>
-                    <button onClick={reset}>Reset</button>
-                    <button onClick={timer ? pause : start}>{timer ? "Pause" : "Start"}</button>
+                    <button onClick={() => reset()}>Reset</button>
+                    <button onClick={state.interval ? pause : start}>{state.interval ? "Pause" : "Start"}</button>
                 </div>
             </div>
             <div className='study-section'>
                 <StudySwitch
-                    studyStatus={studyStatus}
-                    setStudyStatus={setStudyStatus}
+                    studyStatus={state.studyStatus}
+                    setStudyStatus={(value) => dispatch({ type: 'STUDY_STATUS', studyStatus: value })}
                     reset={reset}
                 />
             </div>
