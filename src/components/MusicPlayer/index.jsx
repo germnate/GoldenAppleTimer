1cba0335907240c2d34fd084e0bfd166b6a46632
Nathan Bustamante <bustamanten2@gmail.com>
2024-11-04 01:45:08 -0500

    fix file button

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -15,7 +15,10 @@ export function MusicPlayer() {
 
     return (
         <div className='music-container'>
-            <input type='file' id='fileInput' accept='audio/*' multiple onChange={onChangeFile} />
+            <div className='fileInput-container'>
+                <label htmlFor='fileInput'>Add Media</label>
+                <input type='file' id='fileInput' accept='audio/*' multiple onChange={onChangeFile} />
+            </div>
             <div className='active-song-player music-row active' onClick={musicPlayer.getActiveTrack()?.paused ? musicPlayer.unpause : musicPlayer.pause}>
                 <img src={musicPlayer.getActiveTrack()?.paused ? play : pause} height={30} width={30} />
                 {musicPlayer.getActiveTrack()?.file?.name}
